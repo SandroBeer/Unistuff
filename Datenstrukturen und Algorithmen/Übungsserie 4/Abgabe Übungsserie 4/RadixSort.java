628668051abe7b3f6b5f03875a8fd0282ee07996
AlanAndrey <alan.andrey@students.unibe.ch>
2022-03-23 11:12:32 +0100

    added old radix
    Adda

--- Datenstrukturen und Algorithmen/Übungsserie 4/Abgabe Übungsserie 4/RadixSort.java
+++ Datenstrukturen und Algorithmen/Übungsserie 4/Abgabe Übungsserie 4/RadixSort.java
@@ -95,4 +95,48 @@ public class RadixSort {
             }
         }
     }
+    public static void radixSort(ArrayList<String> A, int d)
+    {
+        ArrayList<LinkedList<String>> queues = new ArrayList<>();
+        // 27 queues for 26 characters plus 'empty' character
+        for (int i=0; i<27; ++i) {
+            queues.add(new LinkedList<String>());
+        }
+
+        // for all positions from right to left
+        for(int j=d-1; j>=0; j--)
+        {
+            // initialize empty queues
+            //for(int i=0; i<27; i++) queues.set(i, new LinkedList<char[]>());
+            for(int i=0; i<27; i++) queues.get(i).clear();
+
+            // place each character array in correct queue
+            for(int i=0; i<A.size(); i++)
+            {
+                if(j<A.get(i).length())
+                {
+                    // characters 'a'-'z'
+                    queues.get(A.get(i).charAt(j)-'a'+1).addLast(A.get(i));
+                }
+                else
+                {
+                    // character array is shorter than current position.
+                    // place it in 'empty' queue. 'emtpy' queue is queue 0
+                    // to get lexicographically correct results, i.e., a<ab.
+                    queues.get(0).addLast(A.get(i));
+                }
+            }
+
+            // traverse queues
+            int n = 0;
+            for(int i=0; i<27; i++)
+            {
+                while(queues.get(i).size() > 0)
+                {
+                    A.set(n, queues.get(i).removeFirst());
+                    n++;
+                }
+            }
+        }
+    }
 }
