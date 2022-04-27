0dac6a3e5ca6b2e993a7b3bd113645a325ea2b06
SandroBeer <sandro.beer@students.unibe.ch>
2022-04-27 09:19:18 +0200

    hoi alan

--- Datenbanken/Übungsserie 8/Aufgabe 2.sql
+++ Datenbanken/Übungsserie 8/Aufgabe 2.sql
@@ -1,5 +1,5 @@
--- DROP TABLE Futter, Kamin, Karton, Laptop, Hund, Lieblingsplatz, Haustier, "hat-bei", Katze, Person, Halter, Aufpasser;
-/*
+DROP TABLE Futter, Kamin, Karton, Laptop, Hund, Lieblingsplatz, Haustier, "hat-bei", Katze, Person, Halter, Aufpasser, HaustierFutter;
+
 CREATE TABLE Futter (Hersteller VARCHAR(100),
 					 Name VARCHAR(100),
 					 PRIMARY KEY (Hersteller, Name));
@@ -28,13 +28,20 @@ CREATE TABLE "hat-bei" (hid INTEGER,
 					 pid INTEGER,
 					 lid INTEGER,
 					 PRIMARY KEY (hid, pid));
-					 
-CREATE TABLE Lieblingsplatz (lid INTEGER PRIMARY KEY);
 
 CREATE TABLE Katze (hid INTEGER PRIMARY KEY, Dominanz INTEGER, Farbe VARCHAR(100));
 
-CREATE TABLE Personen (pid INTEGER PRIMARY KEY, Name VARCHAR(100), Wohnort VARCHAR(100));
+CREATE TABLE Person (pid INTEGER PRIMARY KEY, Name VARCHAR(100), Wohnort VARCHAR(100));
 
 CREATE TABLE Halter (pid INTEGER PRIMARY KEY, Typ VARCHAR(100));
 
-CREATE TABLE Aufpasser (pid INTEGER PRIMARY KEY, Studenlohn INTEGER);*/
+CREATE TABLE Aufpasser (pid INTEGER PRIMARY KEY, Studenlohn INTEGER);
+
+-- mit dem nächsten Statement wird das fehlende Verknüpfungsattribut bei "gehört" ergänzt
+ALTER TABLE Haustier ADD pid INTEGER;
+
+-- mit diesem Statement wird die fehlende "mag"-Tabelle ergänzt
+CREATE TABLE HaustierFutter (hid INTEGER,
+							 Hersteller VARCHAR(100),
+							 Name VARCHAR(100),
+							 PRIMARY KEY (hid, Hersteller, Name));
